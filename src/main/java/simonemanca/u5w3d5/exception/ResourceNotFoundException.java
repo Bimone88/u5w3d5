e42003e6216c32f89d52ea38cb87d85021261d2c
Simone Manca <simone.manca.98@gmail.com>
2024-04-19 13:03:03 +0200

    add exception

---
+++ src/main/java/simonemanca/u5w3d5/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package simonemanca.u5w3d5.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
