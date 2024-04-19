59ebf1b804eeeb539e0b3640d11b6d1cd55fd1e0
Simone Manca <simone.manca.98@gmail.com>
2024-04-19 13:29:50 +0200

    changes

--- src/main/java/simonemanca/u5w3d5/U5w3d5Application.java
+++ src/main/java/simonemanca/u5w3d5/U5w3d5Application.java
@@ -2,8 +2,10 @@ package simonemanca.u5w3d5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"simonemanca.u5w3d5.model"})
 public class U5w3d5Application {
 
 	public static void main(String[] args) {
